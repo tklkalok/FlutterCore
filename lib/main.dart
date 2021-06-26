34a77f2ff6b16dbda38c7318f6bb46c8a4098696
TONY <tkl.kalok@gmail.com>
2021-06-26 16:35:51 +0800

    Finished codelab lesson two

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ void main() => {runApp(MyApp())};
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Welcome to Flutter', home: RandomWords());
+    return MaterialApp(
+			title: 'Welcome to Flutter',
+			theme: ThemeData(
+				primaryColor: Colors.white,
+			), 
+			home: RandomWords()
+		);
   }
 }
 
@@ -19,12 +25,45 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Startup Name Generator')),
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+          actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
+        ),
         body: _buildSuggestions());
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+			MaterialPageRoute<void>(
+				builder: (BuildContext context){
+					final tiles = _saved.map(
+						(WordPair pair) {
+							return ListTile(
+								title: Text(
+									pair.asPascalCase,
+									style: _biggerFont,
+								)
+							);
+						}
+					);
+					final divided = tiles.isNotEmpty ? 
+						ListTile.divideTiles(context: context, tiles: tiles).toList()
+						:
+						<Widget>[];
+					return Scaffold(
+						appBar: AppBar(
+							title: Text('Saved Suggestions')
+						),
+						body: ListView(children: divided)
+					);
+				}
+			)
+		);
   }
 
   Widget _buildSuggestions() {
@@ -42,10 +81,22 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
         title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ));
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        });
   }
 }
